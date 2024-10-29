77a37d8984b4a7e8d91a0d8e8f4aad7cc3864221
joeychen0516 <chen610409@gmail.com>
2024-10-30 01:39:48 +0800

    update

--- all.js
+++ all.js
@@ -6,7 +6,7 @@ let data=[
         "price":1280,
         "rate":8.6,
         "group":"8",
-        "imgUrl":"/assets/images/travel_1.png",
+        "imgUrl":"./assets/images/travel_1.png",
         "content":"嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合，提供台東綠島來回船票、綠島環島機車、綠島民宿住宿，行程加贈『綠島浮潛體驗』以及『綠島生態導覽』，讓你用輕鬆的綠島套裝自由行，也能深度認識綠島在地文化。"
     },
     {
@@ -16,7 +16,7 @@ let data=[
         "price":2580,
         "rate":8.2,
         "group":"12",
-        "imgUrl":"/assets/images/travel_2.png",
+        "imgUrl":"./assets/images/travel_2.png",
         "content":"清境農場青青草原數十公頃碧草，餵食著數以百計的綿羊和牛群，中央山脈最高的北三段群峰形成一堵如帶的高牆，攔住清晨的薄霧山嵐，成就了從花蓮翻山而來的雲瀑在濁水溪谷積成雲海，這些景觀豐沛了清境觀景步道的風格，也涵養它無可取代的特色。"
     },
     {
@@ -26,7 +26,7 @@ let data=[
         "price":2480,
         "rate":89.2,
         "group":"2",
-        "imgUrl":"/assets/images/travel_3.png",
+        "imgUrl":"./assets/images/travel_3.png",
         "content":"南庄雲水豪華露營車，快來擁有最愜意的露營體驗吧！ 一泊一食，輕鬆享受露營車樂趣。 獨立衛浴與私人戶外露臺。 入住豪華露營車還能使用戶外SPA大眾湯，感受美人湯魅力。"
     },
     {
@@ -36,7 +36,7 @@ let data=[
         "price":880,
         "rate":9.3,
         "group":"限時搶購",
-        "imgUrl":"/assets/images/travel_4.png",
+        "imgUrl":"./assets/images/travel_4.png",
         "content":"山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點，帶您飽覽南投瑰麗的自然環境，體驗變化無窮的地形景觀，喜歡挑戰高空的您一定不可錯過。 （含雙龍瀑布入場券 x2）"
     },
     {
@@ -46,7 +46,7 @@ let data=[
         "price":1280,
         "rate":8.2,
         "group":"5",
-        "imgUrl":"/assets/images/travel_5.png",
+        "imgUrl":"./assets/images/travel_5.png",
         "content":"台中全新親子景點寶熊漁樂碼頭，為知名釣具公司「OKUMA」所創立的觀光工廠。一樓藍白希臘漁村風商店街免費參觀。二樓釣魚故事館則設立全台唯一虛擬釣場，透過導覽讓你知道如何釣魚、魚餌怎麼區分，寓教於樂的台中景點！"
     },
     {
@@ -56,7 +56,7 @@ let data=[
         "price":2480,
         "rate":8.6,
         "group":"8",
-        "imgUrl":"/assets/images/travel_6.png",
+        "imgUrl":"./assets/images/travel_6.png",
         "content":"來自日本最受歡迎的兒童遊樂園《 BearSon Park 熊森公園》於全世界有800多家據點，在全世界、日本及台灣，很多小孩的童年都在遊戲愛樂園裡一同成長，提供兒童一個最富教育性及娛樂性的休憩遊樂天地！"
     },
 ];
